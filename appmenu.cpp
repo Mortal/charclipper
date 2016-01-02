0fc0bd2dce572888d9f536ba383c540ec840abb1
Mathias Rav <rav@cs.au.dk>
2016-01-02 14:02:24 +0100

    Add some more useful characters

--- appmenu.cpp
+++ appmenu.cpp
@@ -7,8 +7,11 @@ std::vector<SearchResult> search(QString input) {
         {QString::fromUtf8("ᵀ"), "transpose"},
         {QString::fromUtf8("σ"), "small sigma"},
         {QString::fromUtf8("θ"), "small theta"},
+        {QString::fromUtf8("δ"), "small delta"},
+        {QString::fromUtf8("∂"), "partial differential"},
         {QString::fromUtf8("∈"), "in / member of"},
         {QString::fromUtf8("ⁿ"), "nth power"},
+        {QString::fromUtf8("∑"), "summation"},
         {input, "copy input"},
     };
 }
